e4799626ecb6c42b65c807b9f35ed655de8abda5
Herand19 <134037284+Herand19@users.noreply.github.com>
2024-04-13 19:13:08 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export default {
     "rpc_url": process.env.RPC_URL,
     "private_key": process.env.PRIVATE_KEY,
     "account_type": process.env.ACCOUNT_TYPE || 'simple',
-    "contract_address": "0x66519FCAee1Ed65bc9e0aCc25cCD900668D3eD49",
+    "contract_address": "0x49345182D578B1b3c7Ed4B2e6eCD8Aef63DD2CA3",
     "function_name": "mintTo",
     "entry_point": "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
 }
