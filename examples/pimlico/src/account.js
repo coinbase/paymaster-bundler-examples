9cf8e2e7644b46d776dd4b582fa25b10ac4c4193
Chris Hunter <chris.hunter@coinbase.com>
2024-09-05 10:30:34 -0400

    Move to newest pimlco version and add erc20

--- examples/pimlico/src/account.js
+++ examples/pimlico/src/account.js
@@ -1,11 +1,13 @@
 
-import { privateKeyToSimpleSmartAccount, signerToSafeSmartAccount, signerToEcdsaKernelSmartAccount } from "permissionless/accounts"
+import { toSimpleSmartAccount, toSafeSmartAccount, toEcdsaKernelSmartAccount } from "permissionless/accounts"
 import { privateKeyToAccount } from "viem/accounts"
 import { http, createPublicClient } from 'viem'
 import config from '../../../config.js';
+import { baseSepolia } from "viem/chains";
 
 const publicClient = createPublicClient({
     transport: http(config.rpc_url),
+    chain: baseSepolia
 });
 
 // To customize the signer, see https://docs.pimlico.io/permissionless/reference/accounts/signerToSimpleSmartAccount
@@ -16,7 +18,7 @@ export const getAccount = async (type) => {
     switch (type) {
         case "simple":
             // EOA signer (private key) and Simple Account
-            const simpleAccount = await privateKeyToSimpleSmartAccount(publicClient, {
+            const simpleAccount = await toSimpleSmartAccount(publicClient, {
                 privateKey: config.private_key,
                 factoryAddress: "0x9406Cc6185a346906296840746125a0E44976454",
                 entryPoint: config.entry_point,
@@ -24,7 +26,7 @@ export const getAccount = async (type) => {
             return simpleAccount
         case "safe":
             // EOA signer (private key) and Safe
-            const safeAccount = await signerToSafeSmartAccount(publicClient, {
+            const safeAccount = await toSafeSmartAccount(publicClient, {
                 entryPoint: config.entry_point,
                 signer: signer,
                 safeVersion: "1.4.1",
@@ -34,9 +36,10 @@ export const getAccount = async (type) => {
             return safeAccount
         case "kernel":
              // EOA signer (private key) and Kernel
-            const kernelAccount = await signerToEcdsaKernelSmartAccount(publicClient, {
-                entryPoint: config.entry_point,
-                signer: signer,
+            const kernelAccount = await toEcdsaKernelSmartAccount({
+                entryPoint: {address: config.entry_point, version: "0.6"},
+                owners: [signer],
+                client: publicClient,
                 // index: 0n, // optional
                 // address: "0x...", // optional, only if you are using an already created account
             })
