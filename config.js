0f709d86cacbae85035d863dff61ff300a4e2470
kevin-chen <kevin.chen@coinbase.com>
2024-11-01 22:28:56 -0400

    chore: Update permissionless package examples

--- config.js
+++ config.js
@@ -9,5 +9,6 @@ export default {
     "account_type": process.env.ACCOUNT_TYPE || 'simple',
     "contract_address": "0x66519FCAee1Ed65bc9e0aCc25cCD900668D3eD49",
     "function_name": "mintTo",
-    "entry_point": "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+    "entry_point": "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+    "entry_point_version": "0.6"
 }
--- examples/pimlico/src/account.js
+++ examples/pimlico/src/account.js
@@ -1,42 +1,56 @@
 
-import { privateKeyToSimpleSmartAccount, signerToSafeSmartAccount, signerToEcdsaKernelSmartAccount } from "permissionless/accounts"
+import { toSimpleSmartAccount, toSafeSmartAccount, toEcdsaKernelSmartAccount } from "permissionless/accounts"
 import { privateKeyToAccount } from "viem/accounts"
 import { http, createPublicClient } from 'viem'
 import config from '../../../config.js';
+import { baseSepolia } from "viem/chains";
 
-const publicClient = createPublicClient({
+const client = createPublicClient({
+    chain: baseSepolia,
     transport: http(config.rpc_url),
 });
 
 // To customize the signer, see https://docs.pimlico.io/permissionless/reference/accounts/signerToSimpleSmartAccount
-const signer = privateKeyToAccount(config.private_key)
+const owner = privateKeyToAccount(config.private_key)
 
 // To use a different account, see https://docs.pimlico.io/permissionless/how-to/accounts/use-simple-account
 export const getAccount = async (type) => {
     switch (type) {
         case "simple":
             // EOA signer (private key) and Simple Account
-            const simpleAccount = await privateKeyToSimpleSmartAccount(publicClient, {
-                privateKey: config.private_key,
+            const simpleAccount = await toSimpleSmartAccount({
+                owner,
+                client,
                 factoryAddress: "0x9406Cc6185a346906296840746125a0E44976454",
-                entryPoint: config.entry_point,
+                entryPoint: {
+                    address: config.entry_point,
+                    version: config.entry_point_version,
+                },
             })
             return simpleAccount
         case "safe":
             // EOA signer (private key) and Safe
-            const safeAccount = await signerToSafeSmartAccount(publicClient, {
-                entryPoint: config.entry_point,
-                signer: signer,
-                safeVersion: "1.4.1",
+            const safeAccount = await toSafeSmartAccount({
+                owner,
+                client,
+                entryPoint: {
+                    address: config.entry_point,
+                    version: config.entry_point_version
+                },
+                version: "1.4.1",
                 // index: 0n, // optional
                 // address: "0x...", // optional, only if you are using an already created account
             })
             return safeAccount
         case "kernel":
              // EOA signer (private key) and Kernel
-            const kernelAccount = await signerToEcdsaKernelSmartAccount(publicClient, {
-                entryPoint: config.entry_point,
-                signer: signer,
+            const kernelAccount = await toEcdsaKernelSmartAccount({
+                owners: [owner],
+                client,
+                entryPoint: {
+                    address: config.entry_point,
+                    version: config.entry_point_version
+                },
                 // index: 0n, // optional
                 // address: "0x...", // optional, only if you are using an already created account
             })
